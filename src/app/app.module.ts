60a6430177cbc31c63f6973d4434a68a84951270
jaydeepliddet <jaydeepliddet@gmail.com>
2022-01-31 12:22:19 +0530

    my studio

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OpenCloseComponent } from './open-close/open-close.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    OpenCloseComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+     BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
